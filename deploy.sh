4203cc6e46db6d7b4a2ff1201ff5d15b63c0ab04
Chris Joakim <christopher.joakim@gmail.com>
2018-07-12 11:29:54 -0700

    added dock link to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Chris Joakim, Microsoft, 2018/07/12
+# see https://docs.microsoft.com/en-us/azure/app-service/app-service-deploy-zip
 #
 # $ ./deploy.sh
 # Enter host password for user 'cjoakim':
